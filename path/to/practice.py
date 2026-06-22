5a8e2fcef894d114f008028cdd0d0c8f467ad9af
Your Name <you@example.com>
2026-06-22 22:41:02 +0500

    Practice of oprators

--- path/to/practice.py
+++ path/to/practice.py
@@ -1,30 +1,29 @@
-# numbers = []
-# operators = []
+numbers = []
+operators = []
 
 statement = input("Enter the statement: ")
-value = eval(statement)
+
+# Separate numbers and operators
+for char in statement:
+    try:
+        numbers.append(int(char))
+    except ValueError:
+        operators.append(char)
+
+# Start with the first number
+value = numbers[0]
+
+# Perform calculations
+for i in range(len(operators)):
+    op = operators[i]
+
+    if op == '+':
+        value += numbers[i + 1]
+    elif op == '-':
+        value -= numbers[i + 1]
+    elif op == '*':
+        value *= numbers[i + 1]
+    elif op == '/':
+        value /= numbers[i + 1]
+
 print("The value is:", value)
-# # Separate numbers and operators
-# for char in statement:
-#     try:
-#         numbers.append(int(char))
-#     except ValueError:
-#         operators.append(char)
-
-# # Start with the first number
-# value = numbers[0]
-
-# # Perform calculations
-# for i in range(len(operators)):
-#     op = operators[i]
-
-#     if op == '+':
-#         value += numbers[i + 1]
-#     elif op == '-':
-#         value -= numbers[i + 1]
-#     elif op == '*':
-#         value *= numbers[i + 1]
-#     elif op == '/':
-#         value /= numbers[i + 1]
-
-# print("The value is:", value)
